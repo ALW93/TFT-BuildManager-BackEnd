767a1c4eb3f4ca0fe0e23f14c9daede402dc5d3e
alycialwang@gmail.com <alycialwang@gmail.com>
2020-10-28 13:42:26 -0400

    🍰 Migration, Model Associations, Seeders All Working

--- db/migrations/20201027232619-create-build-champion-item.js
+++ db/migrations/20201027232619-create-build-champion-item.js
@@ -1,48 +1,36 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('build_champion_items', {
+    return queryInterface.createTable("build_champion_items", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       buildId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: {
-          model: "Builds",
-          key: "id",
-        },
       },
       championId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: {
-          model: "Champions",
-          key: "id",
-        },
       },
       itemId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: {
-          model: "Items",
-          key: "id",
-        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('build_champion_items');
-  }
+    return queryInterface.dropTable("build_champion_items");
+  },
 };
